84cbc6d64c5ef1c111a076ac9f77dfe01cb64f02
arsts <arstsu@gmail.com>
2019-05-20 15:42:08 +0300

    Created parent component

---
+++ src/comopnents/SearchPanel/SearchPanel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class SearchPanel extends Component {
+  render() {
+    return (
+      <div>
+        <label></label>
+        <input type="text" placeholder="Enter search items"></input>
+        <button>Search</button>
+      </div>
+    )
+  }
+}
